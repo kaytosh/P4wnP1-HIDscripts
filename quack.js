6276a3126e0c799c27eaa7898b18ed6abf949274
kajt0sz <46679775+kaytosh@users.noreply.github.com>
2020-09-15 19:23:10 +0200

    Add files via upload

---
+++ quack.js
@@ -0,0 +1,9 @@
+layout('us');
+typingSpeed(0,0)
+
+delay(500);
+press("GUI r");
+delay(500);
+type("notepad\n")
+delay(500);
+type("you just got QUACKED!\n=========================\nsomeone can access your computer by pluging something to it\n=========================\nBE CAREFULL! >_< ")
