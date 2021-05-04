4f401a0b2a4f121f542e9ab993c0378021ad6bf7
kajt0sz <46679775+kaytosh@users.noreply.github.com>
2021-05-04 19:07:31 +0200

    Update gain_powershell_admin_win.js

--- gain_powershell_admin_win.js
+++ gain_powershell_admin_win.js
@@ -1,13 +1,16 @@
+
 layout("us");
 typingSpeed(0,0)
 
 delay(500);
 press("GUI r");
+delay(500);
 type("powershell.exe");
-delay(100);
+delay(500);
 press("CTRL SHIFT ENTER");
 delay(500);
 press("SHIFT TAB");
 press("ENTER");
 //change window size to very small:
+//delay(500);
 //type("mode con:cols=15 lines=1\n");
