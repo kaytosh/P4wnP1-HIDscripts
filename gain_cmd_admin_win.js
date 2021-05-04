d406d9278ca10bc8dedfc4599af061d0319fd2ef
kajt0sz <46679775+kaytosh@users.noreply.github.com>
2021-05-04 19:06:34 +0200

    Update gain_cmd_admin_win.js

--- gain_cmd_admin_win.js
+++ gain_cmd_admin_win.js
@@ -3,11 +3,13 @@ typingSpeed(0,0)
 
 delay(500);
 press("GUI r");
+delay(500);
 type("cmd.exe");
-delay(100);
+delay(500);
 press("CTRL SHIFT ENTER");
 delay(500);
 press("SHIFT TAB");
 press("ENTER");
 //change window size to very small:
+//delay(500);
 //type("mode con:cols=15 lines=1\n");
